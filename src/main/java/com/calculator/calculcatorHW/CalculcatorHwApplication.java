e016e6ab5bd80ad3e3412cd73e049b0b63cdf110
Nazar <epoxasmute@mail.ru>
2024-08-19 19:56:23 +0300

    Git init

---
+++ src/main/java/com/calculator/calculcatorHW/CalculcatorHwApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.calculcatorHW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculcatorHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculcatorHwApplication.class, args);
+	}
+
+}
