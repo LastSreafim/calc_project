c441f85544b87b74e56bab1cf22dd31383cf7995
Nazar <epoxasmute@mail.ru>
2024-08-20 20:52:19 +0300

    Complete HomeWork (Tests)

---
+++ src/main/java/com/calculator/calculcatorHW/exceptions/DivideByZeroException.java
@@ -0,0 +1,8 @@
+package com.calculator.calculcatorHW.exceptions;
+
+public class DivideByZeroException extends IllegalArgumentException{
+
+    public DivideByZeroException(String s) {
+        super(s);
+    }
+}
